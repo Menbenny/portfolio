212b3832818701ab16d478c93452ec87ed3cac95
Menac_B <menacbinyamin@gmail.com>
2024-12-04 13:10:24 +0200

    Navbar - file edit

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -4,6 +4,36 @@ import Nav from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/fa";
+import {
+    AiFillStar,
+    AiOutlineHome,
+    AiOutlineFundProjectionScreen,
+    AiOutlineUser,
+  } from "react-icons/ai";
+  
+  import { CgFileDocument } from "react-icons/cg";
 
 
-function Navbar() {}
+function Navbar() {
+    const [expand, updateExpanded] = useState(false);
+    const [navColour, updateNavbar] = useState(false);
+
+    function scrollHandler() {
+        if (window.scrollY >= 20) {
+            updateNavbar(true);
+        } else {
+            updateNavbar(false);
+        }
+    }
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return (
+        <Navbar
+        
+            
+        >
+
+        </Navbar>
+    )
+}
