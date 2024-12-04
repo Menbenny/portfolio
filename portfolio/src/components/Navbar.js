ab6485f6a8718fcdca2e9b049112172829c88f7c
Menac_B <menacbinyamin@gmail.com>
2024-12-04 13:05:36 +0200

    Footer & NavBar - Content added

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { CgGitFork } from "react-icons/fa";
+
+
+function Navbar() {}
